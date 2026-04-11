6705fec08c9d268c239ce4802671244ec2edc207
anthonylarusso6 <anthony@triplefsports.com>
2026-04-11 16:05:53 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['kluuoibuhkxukbqodfet.supabase.co'],
+  },
+}
+
+module.exports = nextConfig
