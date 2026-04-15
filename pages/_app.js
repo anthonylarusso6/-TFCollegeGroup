c3bd3465f7ff07dbe84512f18ddf036d830a30c2
anthonylarusso6 <anthony@triplefsports.com>
2026-04-15 18:00:43 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
